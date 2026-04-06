6e0e7d0866f4bb0cdb9ce16a6c21707fa898bc8e
David Dávila Vilanova <david.davila@doodle.com>
2026-04-06 09:08:15 -0500

    refactor: Change use int values directly

--- lib/first_day_of_the_week.dart
+++ lib/first_day_of_the_week.dart
@@ -3,39 +3,10 @@ import 'first_day_of_the_week_platform_interface.dart';
 class FirstDayOfTheWeek {
   const FirstDayOfTheWeek({required this.defaultValue});
 
-  final DayOfWeek defaultValue;
+  final int defaultValue;
 
-  Future<DayOfWeek?> getValue() async {
-    final value = await FirstDayOfTheWeekPlatform.instance.get();
-    if (value == null) {
-      return null;
-    }
-    return DayOfWeek.fromValue(value);
-  }
+  Future<int?> getValue() async =>
+      await FirstDayOfTheWeekPlatform.instance.get();
 
-  Future<DayOfWeek> getValueOrDefault() async =>
-      await getValue() ?? defaultValue;
-}
-
-enum DayOfWeek {
-  monday(DateTime.monday),
-  tuesday(DateTime.tuesday),
-  wednesday(DateTime.wednesday),
-  thursday(DateTime.thursday),
-  friday(DateTime.friday),
-  saturday(DateTime.saturday),
-  sunday(DateTime.sunday);
-
-  const DayOfWeek(this.value);
-
-  final int value;
-
-  static DayOfWeek? fromValue(int value) {
-    for (var day in DayOfWeek.values) {
-      if (day.value == value) {
-        return day;
-      }
-    }
-    return null;
-  }
+  Future<int> getValueOrDefault() async => await getValue() ?? defaultValue;
 }
